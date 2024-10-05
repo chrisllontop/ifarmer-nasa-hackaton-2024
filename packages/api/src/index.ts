cb85662fdd737f20b4622b90da8c36fdd2ff1da3
cpajuelodeveloper <cpajuelodeveloper@gmail.com>
2024-10-05 18:22:29 -0500

    Add weather api (#8)

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -5,22 +5,20 @@ import './database/database.setup';
 
 import { usersController } from './modules/user/user.controller';
 import { authController } from './modules/auth/auth.controller';
+import { weatherController } from './modules/weather/weather.controller';
 
 const PORT = process.env.PORT || 3000;
 
 const app = new Elysia()
   .use(swagger())
   .group('/api', (app) =>
-    app.group('/user', (app) =>
-      app.use(usersController)
-    )
-    .group('/auth', (app) =>
-      app.use(authController)
-    )
+    app.use(usersController)
+    .use(authController)
+    .use(weatherController)
     .get('/', 'Hello Elysia', { response: t.String({ description: 'sample description' }) })
   )
-  .listen(PORT, () => {
-    console.log(`🦊 Elysia is running at ${app.server?.hostname}:${PORT}`);
+  .listen(PORT, (app) => {
+    console.log(`🦊 Elysia is running at ${app?.hostname}:${PORT}`);
   });
 
 export type App = typeof app;
--- packages/api/src/modules/user/user.controller.ts
+++ packages/api/src/modules/user/user.controller.ts
@@ -4,24 +4,25 @@ import { createUserDto, updateUserDto } from './user.dto';
 import { authenticator } from '../auth/auth.validator';
 
 export const usersController = (app: Elysia) =>
-  app
-  .use(authenticator)
-  .get('/', async ({ user }) => {
-    return user;
-  })
-  .guard( createUserDto, (app) =>
-    app.post('/', async ({ body }) => {
-      return await User.create(body);
+  app.group('/user', (app) =>
+    app.use(authenticator)
+    .get('/', async ({ user }) => {
+      return user;
+    })
+    .guard( createUserDto, (app) =>
+      app.post('/', async ({ body }) => {
+        return await User.create(body);
+      })
+    )
+    .get('/:id', async ({ params}) => {
+      return await User.findById(params.id);
+    })
+    .guard( updateUserDto, (app) =>
+      app.put('/:id', async ({ params, body }) => {
+        return await User.findByIdAndUpdate(params.id, body);
+      })
+    )
+    .delete('/:id', async ({ params }) => {
+      return await User.findByIdAndDelete(params.id);
     })
   )
-  .get('/:id', async ({ params}) => {
-    return await User.findById(params.id);
-  })
-  .guard( updateUserDto, (app) =>
-    app.put('/:id', async ({ params, body }) => {
-      return await User.findByIdAndUpdate(params.id, body);
-    })
-  )
-  .delete('/:id', async ({ params }) => {
-    return await User.findByIdAndDelete(params.id);
-  })
