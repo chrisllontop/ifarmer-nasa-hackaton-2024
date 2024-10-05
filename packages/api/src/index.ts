5c05394638d9e6fc9f0c1ccd008d82fb57c1e2dc
Christian Llontop <chrisllontop@icloud.com>
2024-10-05 11:39:17 -0500

    Add Elysia eden integration (#2)

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import { Elysia, t } from 'elysia'
+
+const app = new Elysia()
+	.get('/', () => 'Hi Elysia')
+	.get('/id/:id', ({ params: { id } }) => id)
+	.post('/mirror', ({ body }) => body, {
+		body: t.Object({
+			id: t.Number(),
+			name: t.String()
+		})
+	})
+	.listen(3000)
+
+export type App = typeof app;
+
+console.log(
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+)
