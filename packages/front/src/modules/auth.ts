7fe5dac21fc05041b5bd34cbc414e5b354ef21d4
cialese <cinthyaless@gmail.com>
2024-10-05 18:17:50 -0500

    add login call

--- packages/front/src/modules/auth.ts
+++ packages/front/src/modules/auth.ts
@@ -6,7 +6,7 @@ interface LoginBody {
 }
 
 export const login = async (data: LoginBody) => {
-	return await data;
+	return await client.api.auth.auth.login.post(data);
 };
 //
 // export const user = async () => {
