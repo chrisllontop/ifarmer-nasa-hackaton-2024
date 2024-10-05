828ae90d0ee31f0f1a2529636741600abad1d22b
cialese <cinthyaless@gmail.com>
2024-10-05 13:04:40 -0500

    add login form, and biome config

--- packages/front/src/App.tsx
+++ packages/front/src/App.tsx
@@ -1,64 +1,66 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/templates/layout';
-import Home from './components/pages/Home';
-import Alerts from './components/pages/Alerts';
-import Search from './components/pages/Search';
-import Login from './pages/Login';
-import ProtectedRoute from './components/utilities/ProtectedRoute';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import './App.css'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Alerts from "./components/pages/Alerts";
+import Home from "./components/pages/Home";
+import Search from "./components/pages/Search";
+import Layout from "./components/templates/layout";
+import ProtectedRoute from "./components/utilities/ProtectedRoute";
+import Login from "./pages/Login";
 
-const App: React.FC = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(true);
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./App.css";
 
-  useEffect(() => {
-    // Verify session
-    // const auth = localStorage.getItem('isAuthenticated');
-    // setIsAuthenticated(auth === 'true');
-  }, []);
-  console.log({isAuthenticated});
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route
-          path="/crops"
-          element={
-            <ProtectedRoute isAuthenticated={isAuthenticated}>
-              <Layout>
-                <Home />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/alerts"
-          element={
-            <ProtectedRoute isAuthenticated={isAuthenticated}>
-              <Layout>
-                <Alerts />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/search"
-          element={
-            <ProtectedRoute isAuthenticated={isAuthenticated}>
-              <Layout>
-                <Search />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </Router>
-  );
-};
+const theme = createTheme({
+	palette: {
+		mode: "light",
+	},
+});
+
+function App() {
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<Router>
+				<Routes>
+					<Route path="/" element={<Login />} />
+					<Route
+						path="/crops"
+						element={
+							<ProtectedRoute isAuthenticated>
+								<Layout>
+									<Home />
+								</Layout>
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						path="/alerts"
+						element={
+							<ProtectedRoute isAuthenticated>
+								<Layout>
+									<Alerts />
+								</Layout>
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						path="/search"
+						element={
+							<ProtectedRoute isAuthenticated>
+								<Layout>
+									<Search />
+								</Layout>
+							</ProtectedRoute>
+						}
+					/>
+				</Routes>
+			</Router>
+		</ThemeProvider>
+	);
+}
 
 export default App;
--- packages/front/src/pages/Login.tsx
+++ packages/front/src/pages/Login.tsx
@@ -1,40 +1,30 @@
-import React, { useState, FormEvent } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import { LoginForm } from "../components/login/LoginForm.tsx";
 
 const Login: React.FC = () => {
-    const [username, setUsername] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-    const navigate = useNavigate();
+	const [email, setEmail] = useState<string>("test@gmail.com");
+	const [password, setPassword] = useState<string>("password");
+	const navigate = useNavigate();
 
-    const handleLogin = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (username === 'admin' && password === 'password') {
-            navigate('/home');
-        } else {
-            alert('Credenciales incorrectas');
-        }
-    };
+	const handleLogin = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (email === "test@gmail.com" && password === "password") {
+			navigate("/crops");
+		} else {
+			alert("Credenciales incorrectas");
+		}
+	};
 
-    return (
-        <div>
-            <h2>Login</h2>
-            <form onSubmit={handleLogin}>
-                <input
-                    type="text"
-                    placeholder="Username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <button type="submit">Login</button>
-            </form>
-        </div>
-    );
+	return (
+		<LoginForm
+			handleLogin={handleLogin}
+			email={email}
+			setEmail={setEmail}
+			password={password}
+			setPassword={setPassword}
+		/>
+	);
 };
 
 export default Login;
