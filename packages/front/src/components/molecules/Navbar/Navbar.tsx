9c3b31c43c06cc51dd3846fc0dc7c513d1dbd6fa
cialese <cinthyaless@gmail.com>
2024-10-05 13:14:58 -0500

    fix layout

--- packages/front/src/components/molecules/Navbar/Navbar.tsx
+++ packages/front/src/components/molecules/Navbar/Navbar.tsx
@@ -1,37 +1,49 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { Link, useNavigate } from 'react-router-dom';
-import Box from '@mui/material/Box';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
-import SearchIcon from '@mui/icons-material/Search';
-import viteLogo from '../../../assets/psychiatry.svg'
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import SearchIcon from "@mui/icons-material/Search";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import Box from "@mui/material/Box";
+import { useNavigate } from "react-router-dom";
+import viteLogo from "../../../assets/psychiatry.svg";
 
 function Navbar() {
-  const [value, setValue] = React.useState(0);
-  const navigate = useNavigate();
+	const [value, setValue] = React.useState(0);
+	const navigate = useNavigate();
 
-  function handleNavigation (value, newValue) {
-    setValue(newValue)
-    navigate(newValue)
-  }
+	function handleNavigation(value, newValue) {
+		setValue(newValue);
+		navigate(newValue);
+	}
 
-  return (
-    <Box sx={{ width: 500 }} >
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          handleNavigation(event, newValue)
-        }}
-      >
-        <BottomNavigationAction value='/search' label="Search" icon={<SearchIcon />} />
-        <BottomNavigationAction value='/crops' label="Crops" icon={<img src={viteLogo} alt="Vite logo" />} />
-        <BottomNavigationAction value='/alerts' label="Alerts" icon={<NotificationsNoneIcon />} />
-      </BottomNavigation>
-    </Box>
-  );
+	return (
+		<Box sx={{ width: "100%" }}>
+			<BottomNavigation
+				showLabels
+				value={value}
+				onChange={(event, newValue) => {
+					handleNavigation(event, newValue);
+				}}
+			>
+				<BottomNavigationAction
+					value="/search"
+					label="Search"
+					icon={<SearchIcon />}
+				/>
+				<BottomNavigationAction
+					value="/crops"
+					label="Crops"
+					icon={<img src={viteLogo} alt="Vite logo" />}
+				/>
+				<BottomNavigationAction
+					value="/alerts"
+					label="Alerts"
+					icon={<NotificationsNoneIcon />}
+				/>
+			</BottomNavigation>
+		</Box>
+	);
 }
 
 export default Navbar;
--- packages/front/src/components/templates/layout.tsx
+++ packages/front/src/components/templates/layout.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import Navbar from '../molecules/Navbar';
+import { Box } from "@mui/material";
+import React from "react";
+import Navbar from "../molecules/Navbar";
 
 interface LayoutProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  return (
-    <div>
-      <main>{children}</main>
-      <Navbar />
-    </div>
-  );
+	return (
+		<Box
+			sx={{
+				display: "flex",
+				flexDirection: "column",
+				height: "100vh",
+				width: "100vw",
+			}}
+		>
+			<Box component="main" sx={{ flexGrow: 1 }}>
+				{children}
+			</Box>
+			<Navbar />
+		</Box>
+	);
 };
 
 export default Layout;
