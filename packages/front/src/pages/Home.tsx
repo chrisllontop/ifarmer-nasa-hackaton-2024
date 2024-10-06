d0cb51c7d86571f30bc4912972282615c13c4b5c
Cinthya Segura <cinthyaless@gmail.com>
2024-10-06 15:20:33 -0400

    Add crops list (#31)

--- packages/front/src/pages/Home.tsx
+++ packages/front/src/pages/Home.tsx
@@ -1,10 +1,33 @@
-import React from "react";
+import AddIcon from "@mui/icons-material/Add";
+import { Box, Button, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { CropsList } from "../components/crops/CropsList.tsx";
+import { paths } from "../routes/paths.ts";
 
-const Home: React.FC = () => {
+const Home = () => {
+	const navigate = useNavigate();
 	return (
-		<div>
-			<h2>Crops Page</h2>
-		</div>
+		<Box sx={{ width: "350px", margin: "48px auto 0" }}>
+			<Typography
+				variant="h5"
+				component="h1"
+				textAlign="left"
+				sx={{ marginBottom: "48px" }}
+			>
+				Saved crops
+			</Typography>
+			<Box sx={{ display: "flex", flexDirection: "column", gap: "24px" }}>
+				<Button
+					variant="outlined"
+					startIcon={<AddIcon />}
+					fullWidth
+					onClick={() => navigate(paths.onboarding)}
+				>
+					New crop
+				</Button>
+				<CropsList />
+			</Box>
+		</Box>
 	);
 };
 
