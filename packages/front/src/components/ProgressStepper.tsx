c772551b624b0471d8078078e790f573f7ecde91
cialese <cinthyaless@gmail.com>
2024-10-06 09:25:23 -0500

    add progress stepper logic and splash screen

--- packages/front/src/components/ProgressStepper.tsx
+++ packages/front/src/components/ProgressStepper.tsx
@@ -1,11 +1,10 @@
 import { KeyboardArrowLeft } from "@mui/icons-material";
 import { Button, MobileStepper } from "@mui/material";
+import { grey } from "@mui/material/colors";
 import { useProgressStepper } from "../context/ProgressBar.tsx";
 
 interface ProgressStepperProps {
 	steps: number;
-	activeStep: number;
-	setActiveStep: (value: number) => void;
 }
 export const ProgressStepper = ({ steps }: ProgressStepperProps) => {
 	const { activeStep, setActiveStep } = useProgressStepper();
@@ -20,16 +19,22 @@ export const ProgressStepper = ({ steps }: ProgressStepperProps) => {
 			steps={steps}
 			position="static"
 			activeStep={activeStep}
-			sx={{ maxWidth: 320, flexGrow: 1 }}
-			nextButton={<></>}
+			sx={{
+				maxWidth: "400px",
+				padding: 0,
+				justifyContent: "flex-start",
+				gap: "12px",
+			}}
+			nextButton={""}
 			backButton={
 				<Button
 					size="small"
 					onClick={handleBack}
-					disabled={activeStep === steps}
+					disabled={activeStep === 0}
+					sx={{ paddingLeft: 0, color: grey["A400"] }}
 				>
-					<KeyboardArrowLeft />
-					Paso {activeStep - 1}
+					{activeStep > 2 ? <KeyboardArrowLeft /> : ""}
+					Paso {activeStep}
 				</Button>
 			}
 		/>
---
+++ packages/front/src/pages/SplashScreen.tsx
@@ -0,0 +1,33 @@
+import { Box, Button, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { CardImage } from "../components/cards/CardImage.tsx";
+import { paths } from "../routes/paths.ts";
+
+export const SplashScreen = () => {
+	const navigate = useNavigate();
+
+	return (
+		<Box>
+			<Box sx={{ width: "350px", margin: "0 auto", height: "85vh" }}>
+				<Typography
+					variant="h5"
+					component="h1"
+					textAlign="left"
+					sx={{ marginBottom: "48px" }}
+				>
+					First of all
+				</Typography>
+				<CardImage description="Remember to introduce precise information to get a better answer" />
+				<CardImage description="The result obtained is tailored to your specific needs using AI" />
+				<CardImage description="Track your real time reports  on water management and recommendation" />
+			</Box>
+			<Button
+				variant="contained"
+				sx={{ width: "350px", margin: "0 auto" }}
+				onClick={() => navigate(paths.onboarding)}
+			>
+				Let's start
+			</Button>
+		</Box>
+	);
+};
