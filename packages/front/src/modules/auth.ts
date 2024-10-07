866e320d9602c8c0d39c844a9af1ed5a2cd57c7c
Cinthya Segura <cinthyaless@gmail.com>
2024-10-06 23:59:53 -0400

    redirect to login if user is not valid (#43)

--- packages/front/src/modules/auth.ts
+++ packages/front/src/modules/auth.ts
@@ -1,3 +1,4 @@
+import { paths } from "../routes/paths.ts";
 import client from "../utils/api.ts";
 
 interface LoginBody {
@@ -9,4 +10,20 @@ export const login = async (body: LoginBody) => {
 	const { data, error } = await client.api.auth.login.post(body);
 	if (data) return data;
 	throw error.value;
+};
+
+export const user = async () => {
+	const authToken = localStorage.getItem("access_token");
+
+	const { data, error } = await client.api.user.get({
+		headers: {
+			Authorization: `Bearer ${authToken}`,
+		},
+	});
+	if (data) return data;
+	if (error.value) {
+		localStorage.removeItem("access_token");
+		window.location.href = paths.login;
+		throw error.value;
+	}
 };
