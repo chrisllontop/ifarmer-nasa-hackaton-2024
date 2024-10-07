69639673e0549be141b6630e3652f12f2d1e3b7b
cialese <cinthyaless@gmail.com>
2024-10-06 22:57:12 -0500

    redirect to login if user is not valid

--- packages/front/src/hooks/auth-guard.tsx
+++ packages/front/src/hooks/auth-guard.tsx
@@ -1,13 +1,13 @@
-import PropTypes from "prop-types";
-import React, { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { paths } from "../routes/paths.ts";
+import { useGetUser } from "./use-auth.ts";
 
 export default function AuthGuard({ children }: { children: React.ReactNode }) {
 	const navigate = useNavigate();
-
-	const authenticated = localStorage.getItem("access_token");
+	const { data: user, isPending, error } = useGetUser();
 	const [checked, setChecked] = useState(false);
+	const authenticated = localStorage.getItem("access_token");
 
 	const check = useCallback(() => {
 		if (!authenticated || authenticated === "undefined") {
@@ -18,15 +18,20 @@ export default function AuthGuard({ children }: { children: React.ReactNode }) {
 	}, [authenticated, navigate]);
 
 	useEffect(() => {
-		check();
-	}, []);
+		if (!authenticated || authenticated === "undefined") {
+			localStorage.removeItem("access_token");
+			navigate(paths.login);
+		} else if (!isPending && (error || !user)) {
+			localStorage.removeItem("access_token");
+			navigate(paths.login);
+		} else if (!isPending && user) {
+			check();
+		}
+	}, [authenticated, isPending, user, error, check, navigate]);
 
-	if (!checked) {
+	if (!checked || isPending) {
 		return null;
 	}
+
 	return <>{children}</>;
 }
-
-AuthGuard.propTypes = {
-	children: PropTypes.node,
-};
