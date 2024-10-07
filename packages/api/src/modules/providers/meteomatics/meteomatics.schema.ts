02534c577a584005246bf513f1733bcdf85b860a
cpajuelodeveloper <cpajuelodeveloper@gmail.com>
2024-10-06 20:41:32 -0500

    add wind, pressure and radiation predictions

--- packages/api/src/modules/providers/meteomatics/meteomatics.schema.ts
+++ packages/api/src/modules/providers/meteomatics/meteomatics.schema.ts
@@ -6,6 +6,9 @@ export enum PARAMS {
 	RELATIVE_HUMIDITY = "relative_humidity",
 	APARENT_TEMPERTURE = "t_apparent",
 	ELEVATION_TEMPERTURE = "t",
+  CLEAR_SKY_RADIATION = "clear_sky_rad",
+  WIND_SPEED = "wind_speed",
+  PROB_PRESSURE = "pressure"
 }
 
 export enum TEMP_UNITS {
